d8d7774f2d33a64d7ae44abeae0d0ff9b93a238b
latarnik3 <jakub.ryzewski@gmail.com>
2026-05-05 14:24:51 +0200

    Board is now a structure not a single vector. Made and unified functions that check for win in seprate directions into one function checkForWin().

--- main.cpp
+++ main.cpp
@@ -1,113 +1,121 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <stdexcept>
 
 /*
-0 - wolne miejsce
-1 - miejsce zajęte przez kółko
-2 - miejsce zajęte przez krzyżyk
+0 - puste miejsce
+1 - gracz 1 np. kółko
+2 - gracz 2 np. krzyżyk
 */
 
 /*
-[0  1   2]
-[3  4   5]
-[6  7   8]
+
+==== TODO ====
+- rozdzielic na plki nagłówkowe
+- algorytm minmax
+
 */
-std::vector<int> makeBoard(int n){
-    std::vector<int> Board = {};
-    for(int i=0; i<n*n; ++i){
-        Board.push_back(0);
-    }
-    return Board;
+
+struct Board {
+    std::vector<int> vals;
+    int S;
+    int win_condition;
+};
+
+Board makeBoard(int n, int win_condition) {
+    Board b;
+    b.vals = std::vector<int>(n * n, 0); 
+    b.S = n;
+    b.win_condition = win_condition;
+    return b;
 }
 
-int checkForWinHor(const std::vector<int>& Board, int move_index, int S) {
-    int player = Board[move_index];
+int radius(const Board& board, int move_index, int player, int step, int col_step) {
+    int result = 0;
+    int current_index = move_index;
+    
+    while(true) {
+        int next_index = current_index + step; 
+
+        if(next_index < 0 || next_index >= board.vals.size()) {
+            break; 
+        }
+        
+        int current_col = current_index % board.S;
+        int next_col = next_index % board.S;
+        
+        if(next_col - current_col != col_step) {
+            break;
+        }
+        if(board.vals[next_index] != player) {
+            break;
+        }
+
+        result++; 
+        current_index = next_index;
+    }
+    
+    return result;
+}
+
+
+int checkForWin(const Board& board, int move_index) {
+    int player = board.vals[move_index];
 
     if (player == 0) return 0; 
 
-    int counter = 1; 
+    // POZIOM
+    int hor = 1 + radius(board, move_index, player, -1, -1) + radius(board, move_index, player, 1, 1);
+    if(hor >= board.win_condition) return player;
     
-    // w którym wierszu został wykonany ruch.
-    int row = move_index / S; 
+    // PION
+    int ver = 1 + radius(board, move_index, player, -board.S, 0) + radius(board, move_index, player, board.S, 0);
+    if(ver >= board.win_condition) return player;
 
-    // W LEWO
-    int current_index_left = move_index - 1;
-    
-    while(current_index_left >= 0 && (current_index_left / S) == row && Board[current_index_left] == player){
-        counter++;
-        current_index_left--; // dalej w lewo
-    }
+    // PRZEKĄTNA lewa gora-->prawy dol
+    int diag1 = 1 + radius(board, move_index, player, -board.S - 1, -1) + radius(board, move_index, player, board.S + 1, 1);
+    if(diag1 >= board.win_condition) return player;
 
-    // W PRAWO
-    int current_index_right = move_index + 1;
-    
-    while(current_index_right < Board.size() && (current_index_right / S) == row && Board[current_index_right] == player){
-        counter++;
-        current_index_right++; // dalej w prawo
-    }
+    // PRZEKĄTNA lewy dol-->prawa gora
+    int diag2 = 1 + radius(board, move_index, player, -board.S + 1, 1) + radius(board, move_index, player, board.S - 1, -1);
+    if(diag2 >= board.win_condition) return player;
 
-    if(counter >= S){
-        return player;
-    }
-
+    // No winner yet
     return 0;
 }
 
-int checkForWinVer(const std::vector<int>& Board, int move_index, int S) {
-    int player = Board[move_index];
-
-    if (player == 0) return 0; 
-
-    int counter = 1; 
-
-    // W GÓRĘ
-    int current_index_up = move_index - S;
-    
-    while(current_index_up >= 0 && Board[current_index_up] == player){
-        counter++;
-        current_index_up -= S;
-    }
-
-    // W DÓŁ
-    int current_index_down = move_index + S;
-    
-    while(current_index_down < Board.size() && Board[current_index_down] == player){
-        counter++;
-        current_index_down += S;
-    }
-
-    if(counter >= S){
-        return player;
-    }
-
-    return 0;
-}
-
-void makeMove(std::vector<int>& Board, int index, int move){
+void makeMove(Board& board, int index, int move) {
 
     if(move != 1 && move != 2){
         throw std::invalid_argument("Illegal move");
     }
 
-    if(Board[index] == 0){
-        Board[index] = move;
+    if(board.vals[index] == 0){
+        board.vals[index] = move;
     }
-    else if(Board[index] == 1 || Board[index] == 2){
+    else if(board.vals[index] == 1 || board.vals[index] == 2){
         throw std::invalid_argument("Spot already taken");
     }
     else{
-        std::cout<<"Something weird is here: "<<Board[index]<<std::endl;
+        std::cout << "Something weird is here: " << board.vals[index] << std::endl;
     }
 
-    // tutaj sprawdzanie wygranej zaraz po wykonaniu ruchu
+    int winner = checkForWin(board, index);
+    
+    if(winner != 0){
+        std::cout << "Winner: " << winner << std::endl;
+    }
+    else{
+        std::cout << "No winner yet" << std::endl;
+    }
 }
 
-int main(){
-    std::vector<int> Board = makeBoard(3);
-    makeMove(Board, 0, 1);
-    makeMove(Board, 1, 1);
-    makeMove(Board, 3, 2);
-    makeMove(Board, 2, 1);
+int main() {
+    Board myBoard = makeBoard(3, 3);
+    
+    makeMove(myBoard, 0, 1);
+    makeMove(myBoard, 1, 1);
+    makeMove(myBoard, 2, 1); 
+    return 0;
 }
-
